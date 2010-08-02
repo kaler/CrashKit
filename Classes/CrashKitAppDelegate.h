9df3661a86afc4f710221afaaef0f07f2dd141df
Parveen Kaler <pk@smartfulstudios.com>
2010-08-02 14:56:08 -0700

    Initial commit

---
+++ Classes/CrashKitAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  CrashKitAppDelegate.h
+//  CrashKit
+//
+//  Created by Parveen Kaler on 10-08-02.
+//  Copyright Smartful Studios Inc 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CrashKitAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CrashKit
+//
+//  Created by Parveen Kaler on 10-08-02.
+//  Copyright Smartful Studios Inc 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
